a8b49b3109a448eabd065c887de6d08bb1edd2e0
Sebastian Proksch <development@mail.proks.ch>
2022-01-19 03:46:30 +0100

    Ad smoke test for Main

---
+++ server/src/test/java/eu/f4sten/server/MainTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 Delft University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.f4sten.server;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import eu.f4sten.server.core.Plugin;
+
+public class MainTest {
+
+    @Test
+    public void pluginCanBeStarted() {
+        assertFalse(TestPlugin.wasCalled);
+        Main.main(new String[] { "--plugin", TestPlugin.class.getName() });
+        assertTrue(TestPlugin.wasCalled);
+        TestPlugin.wasCalled = false;
+    }
+
+    public static class TestPlugin implements Plugin {
+
+        public static boolean wasCalled = false;
+
+        @Override
+        public void run() {
+            wasCalled = true;
+        }
+    }
+}
