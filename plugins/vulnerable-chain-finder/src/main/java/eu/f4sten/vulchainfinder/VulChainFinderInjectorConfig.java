6d2aed4f2eace146023026e581633d4e0e7fb2db
ashkboos <m.keshani@tudelft.nl>
2022-02-21 16:44:42 +0100

    Implementing basic plugin that consumes from callable index output topic and prints the maven coordinate

---
+++ plugins/vulnerable-chain-finder/src/main/java/eu/f4sten/vulchainfinder/VulChainFinderInjectorConfig.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2021 Delft University of Technology
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.f4sten.vulchainfinder;
+
+import com.fasterxml.jackson.databind.Module;
+import com.google.inject.Binder;
+import com.google.inject.Provides;
+import com.google.inject.multibindings.ProvidesIntoSet;
+import eu.f4sten.infra.IInjectorConfig;
+import eu.f4sten.infra.InjectorConfig;
+import eu.f4sten.infra.json.JsonUtils;
+import eu.f4sten.infra.utils.PostgresConnector;
+import eu.f4sten.infra.utils.Version;
+import eu.f4sten.pomanalyzer.json.CoreJacksonModule;
+import eu.f4sten.vulchainfinder.utils.DatabaseUtils;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
+@InjectorConfig
+public class VulChainFinderInjectorConfig implements IInjectorConfig {
+
+    private VulChainFinderArgs args;
+
+    public VulChainFinderInjectorConfig(VulChainFinderArgs args) {
+        this.args = args;
+    }
+
+    @Override
+    public void configure(Binder binder) {
+        binder.bind(VulChainFinderArgs.class).toInstance(args);
+    }
+
+    @Provides
+    public DatabaseUtils bindDatabaseUtils(PostgresConnector pc, JsonUtils json, Version version) {
+        var c = pc.getNewConnection();
+        var dslContext = DSL.using(c, SQLDialect.POSTGRES);
+        return new DatabaseUtils(dslContext, json, version);
+    }
+
+    @ProvidesIntoSet
+    public Module bindJacksonModule() {
+        return new CoreJacksonModule();
+    }
+}
