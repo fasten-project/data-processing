b15994e31579b7c2cf779be4ae1269b0d7ec9730
ashkboos <m.keshani@tudelft.nl>
2022-02-25 19:48:33 +0100

    Implementing vul chain finder DatabaseUtils and its test

--- plugins/vulnerable-chain-finder/src/main/java/eu/f4sten/vulchainfinder/utils/DatabaseUtils.java
+++ plugins/vulnerable-chain-finder/src/main/java/eu/f4sten/vulchainfinder/utils/DatabaseUtils.java
@@ -16,24 +16,93 @@
 
 package eu.f4sten.vulchainfinder.utils;
 
-import eu.f4sten.infra.json.JsonUtils;
-import eu.f4sten.infra.utils.Version;
-import eu.fasten.core.data.metadatadb.MetadataDao;
+import com.google.gson.reflect.TypeToken;
+import eu.fasten.core.data.FastenURI;
+import eu.fasten.core.data.metadatadb.codegen.tables.Modules;
+import eu.fasten.core.data.vulnerability.Vulnerability;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import org.jooq.DSLContext;
+import org.jooq.Record;
 
 public class DatabaseUtils {
 
     private final DSLContext context;
     private final JsonUtils jsonUtils;
-    private final Version version;
 
-    public DatabaseUtils(DSLContext context, JsonUtils jsonUtils, Version version) {
+    public DatabaseUtils(DSLContext context, JsonUtils jsonUtils) {
         this.context = context;
         this.jsonUtils = jsonUtils;
-        this.version = version;
     }
 
-    protected MetadataDao getDao(DSLContext ctx) {
-        return new MetadataDao(ctx);
+    public DSLContext getContext() {
+        return context;
+    }
+
+    public Map<FastenURI, List<Vulnerability>> selectVulCallablesOf(final Set<Long> depIds) {
+        Map<FastenURI, List<Vulnerability>> vulCallables = new HashMap<>();
+        for (Long depId : depIds) {
+            vulCallables = selectVulCallablesOf(depId);
+        }
+        return vulCallables;
+    }
+
+    public Map<FastenURI, List<Vulnerability>> selectVulCallablesOf(final long depId) {
+
+        final var moduleIds = selectAllModulesOf(depId);
+        if (moduleIds.isEmpty()) {
+            return Collections.emptyMap();
+        }
+        return selectConcurrentlyVulCallablesOf(moduleIds);
+    }
+
+    public Map<FastenURI, List<Vulnerability>> selectConcurrentlyVulCallablesOf(
+        final Set<Long> moduleIds) {
+        Map<FastenURI, List<Vulnerability>> result = new ConcurrentHashMap<>();
+        moduleIds.parallelStream().forEach(moduleId ->
+            context.fetch(selectVulCallablesWhereModuleIdIs(moduleId))
+                .forEach(record -> {
+                    final var vulField = record.get(3);
+                    if (vulField == null) {
+                        return;
+                    }
+                    
+                    final var setType =
+                        new TypeToken<HashMap<String, Vulnerability>>() {}.getType();
+                    Map<String, Vulnerability> vulIdVulObject =
+                        jsonUtils.fromJson(vulField.toString(), setType);
+
+                    result.put(createFastenUri(record), new ArrayList<>(vulIdVulObject.values()));
+                }));
+        return result;
+    }
+
+    public FastenURI createFastenUri(final Record record) {
+        final var uriString =
+            String.format("%s%s$%s%s",
+                "fasten://mvn!", record.get(0), record.get(1), record.get(2));
+        return FastenURI.create(uriString);
+    }
+
+    public String selectVulCallablesWhereModuleIdIs(final Long moduleId) {
+        return "SELECT packages.package_name, package_versions.version, callables.fasten_uri, " +
+            "callables.metadata -> 'vulnerabilities' " +
+            "FROM callables, modules, package_versions, packages " +
+            "where modules.id = callables.module_id " +
+            "and package_versions.id = modules.package_version_id " +
+            "and packages.id = package_versions.package_id " +
+            "and callables.metadata -> 'vulnerabilities' IS NOT NULL " +
+            "and callables.module_id = " + moduleId;
+    }
+
+    public Set<Long> selectAllModulesOf(final long depId) {
+        return context.select(Modules.MODULES.ID).from(Modules.MODULES)
+            .where(Modules.MODULES.PACKAGE_VERSION_ID.eq(depId)).fetch()
+            .intoSet(Modules.MODULES.ID);
     }
 }
