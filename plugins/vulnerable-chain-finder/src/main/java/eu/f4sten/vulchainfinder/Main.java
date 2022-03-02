8091a24a8be0f03ccb779025ca17816541e4dc00
ashkboos <m.keshani@tudelft.nl>
2022-03-02 17:22:01 +0100

    Implement and test the Main class of vulchainfinder plugin

--- plugins/vulnerable-chain-finder/src/main/java/eu/f4sten/vulchainfinder/Main.java
+++ plugins/vulnerable-chain-finder/src/main/java/eu/f4sten/vulchainfinder/Main.java
@@ -1,18 +1,19 @@
 /*
  * Copyright 2021 Delft University of Technology
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package eu.f4sten.vulchainfinder;
 
 import eu.f4sten.infra.AssertArgs;
@@ -24,7 +25,14 @@ import eu.f4sten.infra.kafka.Message;
 import eu.f4sten.infra.kafka.MessageGenerator;
 import eu.f4sten.pomanalyzer.data.MavenId;
 import eu.f4sten.pomanalyzer.data.PomAnalysisResult;
+import eu.f4sten.vulchainfinder.utils.CallableIndexUtils;
 import eu.f4sten.vulchainfinder.utils.DatabaseUtils;
+import eu.f4sten.vulchainfinder.utils.ImpactPropagator;
+import eu.f4sten.vulchainfinder.utils.RestAPIDependencyResolver;
+import eu.fasten.core.merge.CGMerger;
+import eu.fasten.core.vulchains.VulnerableCallChainRepository;
+import java.io.FileNotFoundException;
+import java.net.http.HttpClient;
 import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,8 +40,10 @@ import org.slf4j.LoggerFactory;
 public class Main implements Plugin {
 
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+    public static final HttpClient HTTP_CLIENT = HttpClient.newBuilder().build();
 
     private final DatabaseUtils db;
+    private final CallableIndexUtils ci;
     private final Kafka kafka;
     private final VulChainFinderArgs args;
     private final MessageGenerator msgs;
@@ -41,9 +51,10 @@ public class Main implements Plugin {
     private MavenId curId;
 
     @Inject
-    public Main(DatabaseUtils db, Kafka kafka, VulChainFinderArgs args,
+    public Main(DatabaseUtils db, CallableIndexUtils ci, Kafka kafka, VulChainFinderArgs args,
                 MessageGenerator msgs) {
         this.db = db;
+        this.ci = ci;
         this.kafka = kafka;
         this.args = args;
         this.msgs = msgs;
@@ -52,13 +63,15 @@ public class Main implements Plugin {
     @Override
     public void run() {
         AssertArgs.assertFor(args)
-                .notNull(a -> a.kafkaIn, "kafka input topic")
-                .notNull(a -> a.kafkaOut, "kafka output topic");
+            .notNull(a -> a.kafkaIn, "kafka input topic")
+            .notNull(a -> a.kafkaOut, "kafka output topic");
 
         LOG.info("Subscribing to '{}', will publish in '{}' ...", args.kafkaIn, args.kafkaOut);
-        var msgClass =
-            new TRef<Message<Message<Message<Message<MavenId, PomAnalysisResult>, Object>, Object>,
-                Object>>(){};
+
+        final var msgClass = new TRef<Message<Message<Message<Message
+            <MavenId, PomAnalysisResult>, Object>, Object>, Object>>() {
+        };
+
         kafka.subscribe(args.kafkaIn, msgClass, (msg, l) -> {
             final var pomAnalysisResult = msg.input.input.input.payload;
             curId = extractMavenIdFrom(pomAnalysisResult);
@@ -71,9 +84,43 @@ public class Main implements Plugin {
         }
     }
 
-    private void process() {
-        System.out.println("coordinate: " + curId);
-        LOG.info("Processing {}", curId);
+    public void process() {
+        LOG.info("Processing {}", curId.asCoordinate());
+        final var resolver = new RestAPIDependencyResolver(args.restApiBaseURL, HTTP_CLIENT);
+        final var depIds = resolver.resolveDependencyIds(curId);
+        final var vulDeps = db.selectVulnerablePackagesExistingIn(depIds);
+        if (vulDeps == null ||  vulDeps.isEmpty()) {
+            return;
+        }
+        final var merger = new CGMerger(depIds, db.getContext(), ci.getDao());
+        final var mergedCG = merger.mergeAllDeps();
+        final var idUriMap = merger.getAllUrisFromDB(mergedCG);
+        final var vulCallables = db.selectVulCallablesOf(vulDeps);
+        final var propagator = new ImpactPropagator(mergedCG, idUriMap);
+        propagator.propagateUrisImpacts(vulCallables.keySet());
+        LOG.info("Found {} distinct vulnerable paths", propagator.getImpacts().size());
+
+        if (propagator.getImpacts().isEmpty()) {
+            return;
+        }
+
+        final var vulnerableCallChains =
+            propagator.extractApplicationVulChains(vulCallables, curId);
+        if (vulnerableCallChains.isEmpty()) {
+            return;
+        }
+
+        final var vulRepo = initializeVulRepo();
+        final var productName = String.format("%s:%s", curId.groupId, curId.artifactId);
+        vulRepo.store(productName, curId.version, vulnerableCallChains);
+    }
+
+    private VulnerableCallChainRepository initializeVulRepo() {
+        try {
+            return new VulnerableCallChainRepository(args.vulnChainRepoUrl);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static MavenId extractMavenIdFrom(final PomAnalysisResult pomAnalysisResult) {
@@ -98,10 +145,18 @@ public class Main implements Plugin {
     private Throwable returnCause(final Exception e) {
         final var isRunTime = RuntimeException.class.equals(e.getClass());
         final var causeNotNull = e.getCause() != null;
-        if (isRunTime && causeNotNull){
+        if (isRunTime && causeNotNull) {
             return e.getCause();
         }
         return e;
+    }
+
+    public MavenId getCurId() {
+        return curId;
+    }
+
+    public void setCurId(MavenId curId) {
+        this.curId = curId;
     }
 
 }
