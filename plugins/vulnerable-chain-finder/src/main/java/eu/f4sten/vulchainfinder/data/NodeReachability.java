5e87d910d6eaf55d9564547e228aeb8c0a07af21
ashkboos <m.keshani@tudelft.nl>
2022-03-10 22:10:01 +0100

    test node reachability and add isReachableTarget method

--- plugins/vulnerable-chain-finder/src/main/java/eu/f4sten/vulchainfinder/data/NodeReachability.java
+++ plugins/vulnerable-chain-finder/src/main/java/eu/f4sten/vulchainfinder/data/NodeReachability.java
@@ -1,6 +1,25 @@
+/*
+ * Copyright 2021 Delft University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package eu.f4sten.vulchainfinder.data;
 
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
@@ -9,11 +28,31 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 
 public class NodeReachability {
 
-    public Long targetNode;
-    public final Map<Long, Long> nextStepTowardsTarget = new HashMap<>();
+    public long targetNode;
+    public Map<Long, Long> nextStepTowardsTarget = new HashMap<>();
 
-    public NodeReachability(Long impactingNode) {
+    public NodeReachability(long impactingNode) {
         this.targetNode = impactingNode;
+    }
+
+    public boolean isReachingTarget(long nodeId) {
+        return targetNode == nodeId || nextStepTowardsTarget.containsKey(nodeId);
+    }
+
+    public List<Long> getShortestPath(long nodeId) {
+        if (!isReachingTarget(nodeId)) {
+            throw new InvalidParameterException("Node does not reach target");
+        }
+
+        final var path = new ArrayList<Long>();
+        path.add(nodeId);
+
+        var curNode = nodeId;
+        while (curNode != targetNode) {
+            curNode = nextStepTowardsTarget.get(curNode);
+            path.add(curNode);
+        }
+        return path;
     }
 
     @Override
@@ -28,6 +67,6 @@ public class NodeReachability {
 
     @Override
     public String toString() {
-        return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
     }
 }
