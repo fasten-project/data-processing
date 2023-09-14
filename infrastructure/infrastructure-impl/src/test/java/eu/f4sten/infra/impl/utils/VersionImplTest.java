1de06758a62884c050bde3beedcf09a2dd6c14f0
Build Server <proksch@users.noreply.github.com>
2023-09-14 01:37:29 +0000

    Bump version to 0.0.19-SNAPSHOT

--- infrastructure/infrastructure-impl/src/test/java/eu/f4sten/infra/impl/utils/VersionImplTest.java
+++ infrastructure/infrastructure-impl/src/test/java/eu/f4sten/infra/impl/utils/VersionImplTest.java
@@ -30,7 +30,7 @@ import org.junit.jupiter.api.Test;
 
 public class VersionImplTest {
 
-    private static final String CURRENT_VERSION = "0.0.18";
+    private static final String CURRENT_VERSION = "0.0.19-SNAPSHOT";
     private static String srcVersion;
 
     @BeforeAll
